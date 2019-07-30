eb47c05f0feebbd8069346491dd6ad29c9ef4980
Martha Plantz <maplantz89@gmail.com>
2019-07-30 09:13:26 -0700

    added log out functionality

---
+++ src/components/App/App.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react'
+import { Route, Switch } from 'react-router-dom'
+import Header from '../Header/Header'
+// import PrivateRoute from '../Utils/PrivateRoute'
+// import PublicOnlyRoute from '../Utils/PublicOnlyRoute'
+import ThingListPage from '../../routes/ThingListPage/ThingListPage'
+import ThingPage from '../../routes/ThingPage/ThingPage'
+import LoginPage from '../../routes/LoginPage/LoginPage'
+import RegistrationPage from '../../routes/RegistrationPage/RegistrationPage'
+import NotFoundPage from '../../routes/NotFoundPage/NotFoundPage'
+import './App.css'
+
+class App extends Component {
+  state = { hasError: false }
+
+  static getDerivedStateFromError(error) {
+    console.error(error)
+    return { hasError: true }
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <header className='App__header'>
+          <Header />
+        </header>
+        <main className='App__main'>
+          {this.state.hasError && <p className='red'>There was an error! Oh no!</p>}
+          <Switch>
+            <Route
+              exact
+              path={'/'}
+              component={ThingListPage}
+            />
+            <Route
+              path={'/login'}
+              component={LoginPage}
+            />
+            <Route
+              path={'/register'}
+              component={RegistrationPage}
+            />
+            <Route
+              path={'/thing/:thingId'}
+              component={ThingPage}
+            />
+            <Route
+              component={NotFoundPage}
+            />
+          </Switch>
+        </main>
+      </div>
+    )
+  }
+}
+
+export default App
