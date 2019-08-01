57f9337b2948674acf433aab734af502d89c1f6d
Martha Plantz <maplantz89@gmail.com>
2019-08-01 12:18:10 -0700

    finished adding the registration form

--- src/components/RegistrationForm/RegistrationForm.js
+++ src/components/RegistrationForm/RegistrationForm.js
@@ -1,25 +1,38 @@
 import React, { Component } from 'react'
 import { Button, Input, Required } from '../Utils/Utils'
+//import { isNull } from 'util';
+import AuthApiService from '../../services/auth-api-service';
 
 export default class RegistrationForm extends Component {
   static defaultProps = {
     onRegistrationSuccess: () => {}
   }
-
+  
   state = { error: null }
 
   handleSubmit = ev => {
     ev.preventDefault()
     const { full_name, nick_name, user_name, password } = ev.target
 
-    console.log('registration form submitted')
-    console.log({ full_name, nick_name, user_name, password })
-
-    full_name.value = ''
-    nick_name.value = ''
-    user_name.value = ''
-    password.value = ''
-    this.props.onRegistrationSuccess()
+    this.setState({
+      error: null
+    })
+    AuthApiService.postUser({
+      user_name: user_name.value,
+      password: password.value,
+      full_name: full_name.value,
+      nickname: nick_name.value
+    })
+      .then(user => {
+        full_name.value=''
+        password.value=''
+        user_name.value=''
+        nick_name.value=''
+        this.props.onRegistrationSuccess()
+      })
+      .catch(res => {
+        this.setState({error: res.error})
+      })
   }
 
   render() {
